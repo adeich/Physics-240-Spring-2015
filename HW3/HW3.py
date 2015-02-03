5318df70ff8a022a7f1d1fd5d39224f074105f54
adeich <wasted_surfer@yahoo.com>
2015-02-03 12:24:44 -0800

    got HW3* to turn-in readiness.

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -1,53 +1,85 @@
+# HW3.py Aaron Deich
+
+# Comments on the plot behavior:
+# Generally, when N is small, the Taylor approximation will be less precise.
+# Thus all the graphs tend toward 0% fractional error as N gets large.
+# When x is negative, x**i's error blows up with i, though N! eventually surpasses it.
+# My best guess as to why part (b) improves the error is that when x is negative,
+# S(-x) will have half negative terms (when i is even) and half positive (odd), and these will tend
+# to cancel out, making a smaller sum number.
+
+# Comments on my LaTeX setup:
+# I'm running TeXShop version 3.23. It is working fine.
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Absolute fractional error (takes 2 numbers; returns 1 number).
+# Compute absolute fractional error (takes 2 numbers; returns 1 number).
 def compute_abs_frac_error(true_number, approx_number):
 	if true_number is None or approx_number is None:
 		raise BaseException("got a None for x:{}, N:{}".format(true_number, approx_number))
 	return np.abs((approx_number - true_number)/true_number)
 
+# Creates taylor approximation array (int N) for a given float x.
+def create_approximated_e_array(x, N, problem_part):
+	# create empty array of length N.
+	output_array = np.empty(N)
 
-# Computes Taylor approximation of e**(x).
-def approximate_exp(x, N):
-	total = np.sum([(x**i)/(np.math.factorial(i)) for i in np.arange(N)])
-	return total
+	# Force proper types.
+	x = float(x); N = int(N)
+	
+	# auxiliary function which computes the ith term of the Taylor sum.
+	def compute_sum_element_i(x, i):
+		ith_term = (x**i)/np.math.factorial(i)
+		return ith_term	
 
+	def compute_whole_approx_array(x, N):
+		output_array[0] = compute_sum_element_i(x, 0)
+		for i in range(1, N):
+			output_array[i] = output_array[i-1] + compute_sum_element_i(x, i)
 
-# Computes the array for the abs-frac-error for N=60 and specified x.  
-def create_graphing_data(x):
+		return output_array
+	
+
+	if problem_part == 'a' or x > 0:
+		return compute_whole_approx_array(x, N)
+	elif problem_part == 'b':
+		return 1/compute_whole_approx_array(-x, N)
+	else:
+		raise BaseException('Wrong "problem_part" passed: you passed"{}"'.format(
+			problem_part))
+
+# Computes the dependent and independent axis arrays for
+# the abs-frac-error for N=60 and specified x.  
+def create_graphing_data(x, problem_part='a'):
 	N = 60
 
-	# an array of incremental integer N values.
-	#independent_axis = np.arange(1, N + 1)
-	
-	#x_clones_array = np.empty(N); x_clones_array.fill(x)
+	independent_axis = np.arange(N)
 
-	# an array of absolute fractional error for each N value.
-	#dependent_axis = compute_abs_frac_error(np.exp(x_clones_array), 
-	#	approximate_exp(x_clones_array, independent_axis))
-	
-	# No vectorization here, as I couldn't get it to work.
-	# Sadly, a plain old Python for-loop.
-	independent_axis = np.linspace(1, N, N)
-	dependent_axis = []
-	for n in independent_axis:
-		dependent_axis.append(compute_abs_frac_error(np.exp(x), approximate_exp(x, n)))
+	# Using vectorization here.
+	dependent_axis = compute_abs_frac_error(np.e**x,
+		create_approximated_e_array(x, N, problem_part))
 
-	return (np.array(independent_axis), np.array(dependent_axis))
+	return (independent_axis, dependent_axis)
 	
 # Create 2x2 grid containing 4 plots (one for each x-value).
 def plot_2by2_grid(four_x_values_list):
 
-	plt.suptitle('Approx by Aaron Deich')
+	plt.figure(facecolor='w')
+	plt.suptitle('Absolute Fractional Error of Nth-degree Taylor Sum $e^x$ by Aaron Deich')
 
 	for plot_index in range(4):
-		x1, y1 = create_graphing_data(four_x_values_list[plot_index])
+		x1, ya = create_graphing_data(four_x_values_list[plot_index], problem_part='a')
+		x1, yb = create_graphing_data(four_x_values_list[plot_index], problem_part='b')
 		plt.subplot(2, 2, plot_index + 1)
-		plt.plot(x1, y1)
+		plt.plot(x1, ya, 'b-', label='part a')
+		plt.plot(x1, yb, 'g--', label='part b')
 		plt.title('$x = {}$'.format(four_x_values_list[plot_index]))
-		plt.xlabel('N'); plt.ylabel('Error')
+		plt.xlabel('N'); plt.ylabel('$|S(x,N)-e^x| / e^x$')
+		plt.legend(loc='top right')
+		plt.yscale('log')
 
+	plt.tight_layout()
 	plt.savefig('HW3.png')
 	plt.show()
 
