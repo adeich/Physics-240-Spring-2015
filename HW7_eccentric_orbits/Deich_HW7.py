2257dd73c03256ee2df769e240688710a0d38cce
adeich <wasted_surfer@yahoo.com>
2015-02-17 13:01:00 -0800

    part 2 mostly working. calculates absolute max error of total energy.

--- HW7_eccentric_orbits/Deich_HW7.py
+++ HW7_eccentric_orbits/Deich_HW7.py
@@ -35,83 +35,125 @@ def compute_1_orbit(dt, r0, v0):
 		't_array': np.arange(0, dt*len(r_list), dt)}
 		
 
+# run 1 orbit and derive its various properties.
+def study_orbit(r0_vec, v0_vec, timestep=pc['year_in_seconds']/1e3, print_plot=False, printout=False):
+
+	measurements= {}
+
+	# Perform euler-cromer integration to find r and v for all timesteps in an orbit.
+	orbit_results = compute_1_orbit(timestep, r0_vec, v0_vec)
+
+	measurements['period'] = orbit_results['t_array'][-1]
+
+	# find the perihelion and aphelion (biggest and smallest orbital radius)
+	max_r_mag = 0
+	min_r_mag = np.inf
+	for r_vec in orbit_results['r_array']:
+		magnitude = np.linalg.norm(r_vec)
+		if magnitude > max_r_mag: 
+			max_r_mag = magnitude
+		if magnitude < min_r_mag:
+			min_r_mag = magnitude
+	measurements['r_ap'] = r_ap = max_r_mag
+	measurements['r_per'] = r_per = min_r_mag 
+
+	# Compute eccentricity using perihelion and aphelion.
+	measurements['e_empiric'] = (r_ap - r_per) / (r_ap + r_per)
+	measurements['a_empiric'] = (r_per + r_ap) / 2.
+
+	### Compute eccentricity using specific energy and angular momentum.
+
+	# Reduced mass.
+	mu = (pc['M_sun'] * pc['M_earth']) / (pc['M_sun'] + pc['M_earth'])
+
+	# Specific energy E/m = - GM/r
+	measurements['E_spec'] = E_spec = - pc['G'] * (pc['M_earth'] + pc['M_sun']) / (2 * measurements['a_empiric'])
+
+	# Specific angular momentum L = r cross v / mu
+	measurements['L_spec'] = L_spec = np.cross(orbit_results['r_array'][0], orbit_results['v_array'][0]) / mu
+
+	# Eccentricity, computed analytically.
+	measurements['e_analytic'] = np.sqrt(1 + (2 * E_spec * L_spec**2)/(pc['G']**2 * pc['M_sun']**2))		
+
+	# Part 1b.
+	# Does this agree with Kepler's Third Law?
+	# From T**2 / r**3 = 4pi**2 / GM
+	true_value = (4 * np.pi**2) / (pc['G'] * pc['M_sun'])
+	measured_value = measurements['period']**2 / measurements['a_empiric']**3 
+	measurements['KIIIerror'] = (true_value - measured_value) / true_value
+
+	# calc magnitude for 2d array.
+	def get_magnitudes(input_array):
+		return np.sqrt(input_array[:,0]**2 + input_array[:,1]**2)
+
+	# Part 1c.
+	# Is the virial theorem obeyed?
+	kinetic_array = 0.5 *  get_magnitudes(orbit_results['v_array'])**2
+	potential_array = - pc['G'] * pc['M_sun'] / get_magnitudes(orbit_results['r_array'])
+
+
+	if printout:
+		print 'kinetic: ' + str(kinetic_array[-1])
+		print ' ' + str(potential_array[-1])
+
+		print np.mean(kinetic_array), - np.mean(potential_array) / 2.
+
+	total_E_array = kinetic_array + potential_array
+	measurements['E_mean'] = np.mean(total_E_array)
+	measurements['E_std'] = np.std(total_E_array)
+	measurements['E_max'] = np.max(total_E_array)
+	measurements['E_min'] = np.min(total_E_array)
+	measurements['E_abs_max'] = np.max(np.abs(total_E_array))
+
+
+	output_lines = ['r0: {}; v0: {}'.format(r0_vec, v0_vec)]
+	for key, value in measurements.iteritems():
+		output_lines.append('\t{}: {:.2e}'.format(key, value))
+
+	if printout:
+		print '\n'.join(output_lines)
+		
+	if print_plot:
+		fig = plt.figure()
+		ax = fig.add_subplot(111)
+		ax.plot(orbit_results['t_array'], kinetic_array, label='kinetic')
+		ax.plot(orbit_results['t_array'], potential_array, label='potential')
+		ax.plot(orbit_results['t_array'], total_E_array, label='total E')
+		ax.legend(loc='upper right')
+		plt.show()
+
+	return measurements
+	
 # Compute eccentricity, perihelion, and semi-major axis and compare them to 
 # analytic values.
 def do_part1():
 
-	def study_orbit(r0_vec, v0_vec):
+	print 'Part 1:' 
 
-		measurements= {}
-
-		# Perform euler-cromer integration to find r and v for all timesteps in an orbit.
-		orbit_results = compute_1_orbit(pc['year_in_seconds']/1e3, r0_vec, v0_vec)
-
-		measurements['period'] = orbit_results['t_array'][-1]
-	
-		# find the perihelion and aphelion (biggest and smallest orbital radius)
-		max_r_mag = 0
-		min_r_mag = np.inf
-		for r_vec in orbit_results['r_array']:
-			magnitude = np.linalg.norm(r_vec)
-			if magnitude > max_r_mag: 
-				max_r_mag = magnitude
-			if magnitude < min_r_mag:
-				min_r_mag = magnitude
-		measurements['r_ap'] = r_ap = max_r_mag
-		measurements['r_per'] = r_per = min_r_mag 
-
-		# Compute eccentricity using perihelion and aphelion.
-		measurements['e_empiric'] = (r_ap - r_per) / (r_ap + r_per)
-		measurements['a_empiric'] = (r_per + r_ap) / 2.
-
-		### Compute eccentricity using specific energy and angular momentum.
-
-		# Reduced mass.
-		mu = (pc['M_sun'] + pc['M_earth']) / (pc['M_sun'] + pc['M_earth'])
-
-		# Specific energy E/m = - GM/r
-		measurements['E_spec'] = E_spec = - pc['G'] * (pc['M_earth'] + pc['M_sun']) / (2 * measurements['a_empiric'])
-
-		# Specific angular momentum L = r cross v / mu
-		measurements['L_spec'] = L_spec = np.cross(orbit_results['r_array'][0], orbit_results['v_array'][0]) / mu
-
-		# Eccentricity, computed analytically.
-		measurements['e_analytic'] = np.sqrt(1 + (2 * E_spec * L_spec**2)/(pc['G']**2 * pc['M_sun']**2))		
-
-		# From T**2 / r**3 = 4pi**2 / GM
-		true_value = (4 * np.pi**2) / (pc['G'] * pc['M_sun'])
-		measured_value = measurements['period']**2 / measurements['a_empiric']**3 
-		measurements['KIIIerror'] = (true_value - measured_value) / true_value
-
-		output_lines = ['r0: {}; v0: {}'.format(r0_vec, v0_vec)]
-		for key, value in measurements.iteritems():
-			output_lines.append('\t{}: {:.2e}'.format(key, value))
-
-		print '\n'.join(output_lines)
-		
-
-	print 'Part 1a: eccentricity and semi-major axis.'
-	
 	# Near-circular orbit. These are just Earth orbit parameters.
 	study_orbit(r0_vec=np.array([pc['r_earth_sun'], 0]), v0_vec=np.array([0, pc['v_earth']]))
 
 	# slightly more eccentric orbit.
 	study_orbit(r0_vec=np.array([pc['r_earth_sun'], 0]), v0_vec=np.array([0, pc['v_earth'] * 0.8 ]))
 
+	study_orbit(r0_vec=np.array([pc['r_earth_sun'], 0]), v0_vec=np.array([0, pc['v_earth'] * 0.24 ]),
+		print_plot=True)
 	
-def do_part2a():
-	pass
+def do_part2():
 
-def do_part2b():
-	pass
-
-
+	for year_fraction in np.linspace(100000, 100000, 1):
+		orbit_info = study_orbit(r0_vec=np.array([pc['r_earth_sun'], 0]), v0_vec=np.array([0, pc['v_earth'] * 0.24 ]),
+			timestep=pc['year_in_seconds']/year_fraction, printout=False, print_plot=True)
+		abs_max_E= orbit_info['E_abs_max']	
+		mean_E = orbit_info['E_mean']
+		E_error = (abs_max_E - np.abs(mean_E)) / np.abs(mean_E)
+		print 'year_frac: {}\nE_error: {}'.format(year_fraction, E_error)
+	
 
 
 def do_all_of_HW7():
 	do_part1()
-	do_part2a()
-	do_part2b()
+	do_part2()
 
 
 do_all_of_HW7()
