3324444c8a52964d16474a2b802a7fb269be1d1a
adeich <wasted_surfer@yahoo.com>
2015-04-21 23:51:54 -0700

    all working perfectly

--- HW19_Faraday_cage/HW19Deich.py
+++ HW19_Faraday_cage/HW19Deich.py
@@ -58,23 +58,66 @@ def relax(N_gridpoints, L, boundary_vectors, cage_points=[]):
 	return phi, nth_fractional_change
 
 	
-def make_3d_plot(phi, title):
-	
+def make_and_save_plots(phi, change_vec, title):
+	print 'plotting {}.'.format(title)	
+
+
 	N = len(phi[0])
 	fig = plt.figure()
 	ax = fig.gca(projection='3d')
 	X, Y = np.meshgrid(np.arange(N), np.arange(N))
 	cs = max(2, N/40)
 	rs = max(2, N/40)
-	print 'shape(X): {}, shape(Y): {}, shape(phi): {}'.format(np.shape(X), np.shape(Y),
-		 np.shape(phi))
 	ax.plot_surface(X,Y,phi,cstride=cs,rstride=rs,cmap='jet')
 	plt.xlabel('$x$')
 	plt.ylabel('$y$')
 	ax.set_zlabel('$\Phi(x,y)$')
 	plt.title(title)
 	plt.tight_layout()
-	plt.show()
+	#plt.show()
+	plt.savefig('{}_3d.png'.format(title))
+
+	fig2 = plt.figure()
+	plt.plot(np.arange(len(change_vec)), change_vec)
+	plt.grid(True)
+	plt.xlabel('Iteration $N$')
+	plt.ylabel('Fractionial change')
+	plt.title('{} Fractional change per iteration'.format(title))
+	plt.yscale('log')
+	plt.savefig('{}_2d_change.png'.format(title))
+
+	fig3 = plt.figure()
+	plt.plot(np.arange(len(phi[0])), phi[29])
+	plt.xlabel('$x$')
+	plt.ylabel('$\Phi(x,y)|_{y=29}$')
+	plt.title('{} Potential along $y=29$'.format(title))
+	plt.savefig('{}_2d_crosssection.png'.format(title))
+
+
+def make_plot_compare_all(phi_list, change_list, title_list):
+	
+	fig2 = plt.figure()
+	for change_vec, title in zip(change_list, title_list):
+		plt.plot(np.arange(len(change_vec)), change_vec, label=title)
+	plt.grid(True)
+	plt.xlabel('Iteration $N$')
+	plt.ylabel('Fractionial change')
+	plt.title('{} Fractional change per iteration'.format(title))
+	plt.yscale('log')
+	plt.legend()
+	plt.savefig('All_2d_change.png')
+
+	fig3 = plt.figure()
+	for phi, title in zip(phi_list, title_list):
+		plt.plot(np.arange(len(phi[0])), phi[29], label=title)
+	plt.xlabel('$x$')
+	plt.ylabel('$\Phi(x,y)|_{y=29}$')
+	plt.title('{} Potential along $y=29$'.format(title))
+	plt.legend()
+	plt.savefig('All_2d_crosssection.png')
+
+
+
 
 # Each cage will generate two files. Those files are named the following.
 def make_data_filename(basename, phi_or_change=None):
@@ -87,7 +130,7 @@ def make_data_filename(basename, phi_or_change=None):
 		raise BaseException('invalid!')
 	return filename
 
-def compute_different_phi_save_to_file():
+def compute_different_phi_save_to_file(runs_dictionary):
 	N_gridpoints_per_side = 60
 	boundary_vectors_dict = {
 		'x=0': np.zeros(N_gridpoints_per_side),
@@ -95,38 +138,52 @@ def compute_different_phi_save_to_file():
 		'y=0': np.linspace(0, 100, N_gridpoints_per_side),
 		'y=L': np.linspace(0, 100, N_gridpoints_per_side)
 		}
+	for cagekey, cage in runs_dictionary.iteritems():
+		print 'computing phi for {} ...'.format(cagekey)
+		phi_final, nth_fractional_change = relax(N_gridpoints=N_gridpoints_per_side, L=1.,
+		boundary_vectors=boundary_vectors_dict, cage_points=cage)
+		np.savetxt(fname=make_data_filename(cagekey, 'phi'), X=phi_final)
+		np.savetxt(fname=make_data_filename(cagekey, 'change'), X=nth_fractional_change)
+		print '... saved'
+
+
+def load_files_and_make_plots(runs_dictionary):
+
+	phi_list, change_list, title_list = [], [], []
+
+	for cagekey, cage in runs_dictionary.iteritems():
+		phi_filename = make_data_filename(cagekey, phi_or_change='phi')
+		change_filename = make_data_filename(cagekey, phi_or_change='change')
+
+		phi = np.loadtxt(phi_filename)
+		change_vector = np.loadtxt(change_filename)
+
+		phi_list.append(phi)
+		change_list.append(change_vector)
+		title_list.append(cagekey)
+
+		# Make individual plots.
+		make_and_save_plots(phi, change_vector, title=cagekey)
+
+	# Make set of comparative plots.
+	make_plot_compare_all(phi_list, change_list, title_list)
+
+	
+def main():
+
 	runs_dictionary = {
 		'no_cage': [],
 		'cage_a': [(19, 19), (29, 19), (39, 19), (19, 29), (19, 39), (29, 39), (39, 29), (39, 39)],
 		'cage_b': [(19, 19), (19, 39), (39, 19), (39, 39)],
 		'cage_c': [(19,29), (29,19), (39,29), (29, 39)]
 	}
-	for cagekey, cage in runs_dictionary.iteritems():
-		print 'computing phi for {} ...'.format(cagekey)
-		phi_final, nth_fractional_change = relax(N_gridpoints=60, L=1.,
-		boundary_vectors=boundary_vectors_dict, cage_points=cage)
-		np.savetxt(fname=make_data_filename(cagekey, 'phi'), X=phi_final)
-		np.savetxt(fname=make_data_filename(cagekey, 'change'), X=nth_fractional_change)
-		print '... saved'
 
-	
-def main():
-	N_gridpoints_per_side = 60
-	boundary_vectors_dict = {
-		'x=0': np.zeros(N_gridpoints_per_side),
-		'x=L': np.ones(N_gridpoints_per_side) * 100,
-		'y=0': np.linspace(0, 100, N_gridpoints_per_side),
-		'y=L': np.linspace(0, 100, N_gridpoints_per_side)
-		}
-	cage_a = [(19, 19), (29, 19), (39, 19), (19, 29), (19, 39), (29, 39), (39, 29), (39, 39)]
-	cage_b = [(19, 19), (19, 39), (39, 19), (39, 39)]
-	cage_c = [(19,29), (29,19), (39,29), (29, 39)]
-	phi_final, nth_fractional_change = relax(N_gridpoints=N_gridpoints_per_side, L=1.,
-		boundary_vectors=boundary_vectors_dict, cage_points=cage_a )
-	print phi_final
-	make_3d_plot(phi_final, 'plot')
-	return phi_final, nth_fractional_change
+	# Run this to compute phi in the first place.
+	# Once it's run, you can comment it out.
+	compute_different_phi_save_to_file(runs_dictionary)
+
+	load_files_and_make_plots(runs_dictionary)
 
 
 if __name__ == '__main__':
-	compute_different_phi_save_to_file()
+	main()
