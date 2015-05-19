eb89b367c99337df856d666ef22280b7df3f81e9
adeich <wasted_surfer@yahoo.com>
2015-05-18 20:17:23 -0700

    basic instrument identification via machine learning now functional. added four instruments training recordings.

--- HW00_final_project/code/machinelearning.py
+++ HW00_final_project/code/machinelearning.py
@@ -1,5 +1,7 @@
+import numpy as np
 from sklearn import datasets
 from sklearn import svm
+import pickle
 
 
 def make_data_array_for_ML(list_of_sample_dicts):
@@ -12,6 +14,28 @@ def make_data_array_for_ML(list_of_sample_dicts):
 		all_target_list.append(sample_dict['target'])
 
 	return np.array(all_data_list), np.array(all_target_list)
+
+def compile_classification(pickle_filename, data_array, target_array):
+	
+	classification = svm.SVC(gamma=0.001, C=100)
+	classification.fit(data_array, target_array)
+	with open(pickle_filename, 'w') as f:
+		pickle.dump(classification, f)
+	print('Machine learning model written to {}'.format(pickle_filename))
+	
+
+
+def simple_classification(data_array, target_array, vector_in_question):
+
+	classification = svm.SVC(gamma=0.001, C=100)
+	classification.fit(data_array, target_array)
+	print(classification.predict(vector_in_question))
+
+def classify_from_pickle(pickled_filepath, normalized_spectrum_path):
+	with open(pickled_filepath, 'r') as p:
+		classification = pickle.load(p)
+	with open(normalized_spectrum_path, 'r') as test_item:
+		return classification.predict(np.loadtxt(test_item))
 
 
 def simple_test():
--- HW00_final_project/code/main.py
+++ HW00_final_project/code/main.py
@@ -1,14 +1,28 @@
+import numpy as np
 import os
 import re
 import argparse
 import process_sound
 import names_generator
+import machinelearning
 
 filename_dict = {
-	'pickle_filename': 'hello.txt'
+	'sound_samples': '../sound_samples',
+	'intermediate_results': 'intermediate_results',
+	'questionable_sounds': 'questionable_sounds'
 }
+filename_dict['pickle_filename'] = os.path.join(filename_dict['intermediate_results'],
+	'trained_pickle')
 
 
+def get_all_wav_files(source_directory):
+	list_of_files = []
+	for filename in os.listdir(source_directory):
+		if os.path.splitext(filename)[1].lower() == '.wav':
+			list_of_files.append(os.path.join(source_directory, filename)) 
+	return list_of_files
+
+	
 # High-level function which reads in all sound files, 
 # finds their peaks and generates meta-data. A sound file's
 # instrument classification is read from the filename e.g.
@@ -19,39 +33,79 @@ def analyze_all_sound_samples(source_directory=None, list_of_files=None):
 	if not list_of_files:
 		if not source_directory:
 			raise BaseException("needs a source directory!")
-		list_of_files = []
-		for filename in os.listdir(source_directory):
-			if os.path.splitext(filename)[1].lower() == '.wav':
-				list_of_files.append(os.path.join(source_directory, filename)) 
-			
+		list_of_files = get_all_wav_files(source_directory)
+		
 	# analyze each sound file.
 	for file_number, sound_file in enumerate(list_of_files):
 		print('Processing file {}/{} ...'.format(file_number, len(list_of_files)))	
 		process_sound.analyze_one_sound_file(sound_file, names_generator.names_generator,
-			destination_dir='intermediate_results')
+			destination_dir=filename_dict['intermediate_results'])
 
 	print('done.')
 
 
-def prepare_machine_learning(source_directory):
-	pass
+
+# Read spectrum data files and return data formatted for classification.
+def prepare_classification_data(source_directory, intermediate_directory):
+	
+	# get list of original wav files.	
+	list_of_wav_files = get_all_wav_files(source_directory)
+	data_list = []
+	target_list = []
+	
+
+	# for each original wav file, load its normalized spectrum.
+	for wav_file in list_of_wav_files:
+		names_for_this_file = names_generator.names_generator(wav_file, 
+			destination_directory=filename_dict['intermediate_results'])
+		normalized_spectrum_name = names_for_this_file['normalized_spectrum']
+		classification_string = names_for_this_file['classification']
+
+		if len(np.loadtxt(normalized_spectrum_name)) == 5000:
+			data_list.append(np.loadtxt(normalized_spectrum_name))
+			target_list.append(classification_string)
+		else:
+			print("File '{}' is of length {}!".format(normalized_spectrum_name,
+				len(np.loadtxt(normalized_spectrum_name))))
+
+	print('set of classifications: {}'.format(set(target_list)))
+
+	return {'data': np.array(data_list),
+		'target': np.array(target_list) 
+	}	
+		
 
 
-
-def main():
-	pass
 
 
 if __name__ == "__main__":
   # Get commandline arguments.
 	parser = argparse.ArgumentParser(description='description')
 	parser.add_argument('-process', action='store_true',
-                  help='process all wav files.')
-	parser.add_argument('-mltrain', action='store_true',
-                  help='train the machine learning.')
+		help='process all wav files.')
+	parser.add_argument('-mltrain', action='store_true', 
+		help='train the machine learning.')
+	parser.add_argument('-classify', type=str,
+		help='compare 1 specified sound file.')
 	args = parser.parse_args()
 
 	if args.process:
-		analyze_all_sound_samples(source_directory='../sound_samples')	
-	if args.mltrain:
-		prepare_machine_learning()
+		analyze_all_sound_samples(source_directory=filename_dict['sound_samples'])	
+	elif args.mltrain:
+		classification_data = prepare_classification_data(
+		source_directory=filename_dict['sound_samples'],
+		intermediate_directory=filename_dict['intermediate_results'])
+		machinelearning.compile_classification(pickle_filename=filename_dict['pickle_filename'],
+			data_array=classification_data['data'],
+			target_array=classification_data['target']) 
+	elif args.classify:
+		process_sound.analyze_one_sound_file(sound_filename=args.classify,
+			names_generator_function=names_generator.names_generator,
+			destination_dir=filename_dict['questionable_sounds'])
+		names = names_generator.names_generator(original_wav_filename=args.classify,
+			destination_directory=filename_dict['questionable_sounds'])
+		print(machinelearning.classify_from_pickle(
+			pickled_filepath=filename_dict['pickle_filename'],
+			normalized_spectrum_path=names['normalized_spectrum']))
+	
+		
